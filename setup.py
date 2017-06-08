4cf08348684592fd8be8dbf735cdf4a483322342
Loic Jaquemet <loic.jaquemet+github@gmail.com>
2017-06-08 16:45:17 -0600

    fix Readme

--- setup.py
+++ setup.py
@@ -19,21 +19,13 @@ class PyPrepTestsCommand(distutils.cmd.Command):
 
     description = 'Run tests and dumps memory'
     user_options = []
-    #    # The format is (long option, short option, description).
-    #    ('pylint-rcfile=', None, 'path to Pylint _target_platform file'),
-    # ]
 
     def initialize_options(self):
         """Set default values for options."""
-    #  # Each user option must be listed here with their default value.
-    #  self.pylint_rcfile = ''
         pass
 
     def finalize_options(self):
         """Post-process options."""
-    #  if self.pylint_rcfile:
-    #    assert os.path.exists(self.pylint_rcfile), (
-    #        'Pylint _target_platform file %s does not exist.' % self.pylint_rcfile)
         pass
 
     def run(self):
@@ -52,28 +44,25 @@ setup(name="haystack-reverse",
       version="0.40",
       description="Reverse C Structures from a process' memory",
       long_description=open("README.md").read(),
-      url="http://packages.python.org/haystack/",
-      download_url="http://github.com/trolldbois/python-haystack/tree/master",
+      url="http://packages.python.org/haystack-reverse/",
+      download_url="http://github.com/trolldbois/python-haystack-reverse/tree/master",
       license="GPL",
       classifiers=[
         "Topic :: System :: Networking",
         "Topic :: Security",
         "Environment :: Console",
-        "Environment :: X11 Applications :: Qt",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: GNU General Public License (GPL)",
         "Programming Language :: Python",
         "Development Status :: 4 - Beta",
         # "Development Status :: 5 - Production/Stable",
       ],
-      keywords=["memory", "analysis", "forensics", "record", "struct", "ptrace", "reverse", "heap", "lfh", "lal"],
+      keywords=["memory", "analysis", "forensics", "record", "struct", "reverse", "heap"],
       author="Loic Jaquemet",
       author_email="loic.jaquemet+python@gmail.com",
-      packages=[
-                "haystack.reverse",
+      packages=["haystack.reverse",
                 "haystack.reverse.heuristics",
-],
-      # package_dir={"haystack.reverse": 'haystack/reverse'},
+      ],
       package_data={"haystack.reverse.heuristics": ['data/words.100'],},
       entry_points={
           'console_scripts': [
@@ -91,24 +80,15 @@ setup(name="haystack-reverse",
       # https://github.com/numpy/numpy/issues/2434
       # numpy is already installed in travis-ci
       # setup_requires=["numpy"],
-      # search: install requires only pefile, python-ptrace for memory-dump
       # reverse: install requires networkx, numpy, Levenshtein for signatures
-      install_requires=[ "haystack",
-                        # reverse need these.
+      install_requires=["haystack",
                         "numpy",
                         "networkx",
                         "python-Levenshtein"],
       dependency_links=[
-                        # "https://github.com/trolldbois/ctypeslib/tarball/dev#egg=ctypeslib2-2.4beta",
-                        "https://github.com/volatilityfoundation/volatility/tarball/master#egg=volatility-trunk",
-                        "https://github.com/google/rekall/tarball/master#egg=rekall-trunk",
-                        "https://github.com/erocarrera/pefile/archive/pefile-1.2.10-139.tar.gz"],
-      # build_test_requires = ["ctypeslib2>=2.1.3"],
+                        "https://github.com/trolldbois/python-haystack/tarball/development#egg=haystack",
+                        ],
       test_suite="test.alltests",
-      # https://pythonhosted.org/setuptools/setuptools.html
-      # prep_test requires ctypeslib2
-      # tests_require=["volatility"],
-      # tests_require=["ctypeslib2>2.1.3"],
       cmdclass={
           'preptests': PyPrepTestsCommand,
       },
